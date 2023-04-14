6a1b86fa3d4271bafbcc2dbdb4862c9cebc6ed06
Aron Adams <aronrapetto@hotmail.com>
2023-04-14 15:30:57 -0300

    style: app global style

---
+++ src/styles/global.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus-visible {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${props => props.theme['yellow-dark']};
+  }
+
+  body {
+    background: ${props => props.theme.background};
+    color: ${props => props.theme['base-text']};
+  }
+
+  body, input, textarea, button {
+    font-size: 1rem;
+    line-height: 1.6;
+    font-family: 'roboto', sans-serif;
+  }
+`
