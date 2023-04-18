54c751e7f2a30c31622f822e7c06d295d2ddcb90
Aron Adams <aronrapetto@hotmail.com>
2023-04-18 16:39:05 -0300

    feat: add Delivery info to context app

--- src/context/CoffeContext.tsx
+++ src/context/CoffeContext.tsx
@@ -12,15 +12,31 @@ interface CartItem {
   qtd: number
 }
 
+interface DeliveryInformations {
+  cep: string
+  street: string
+  number: string
+  complement?: string
+  district: string
+  city: string
+  uf: string
+  payment: string
+}
+
 interface CoffeContextType {
   shoppingCart: CartItem[]
   addItemToShoppingCart: (newItem: CartItem) => void
+  deliveryInfo: DeliveryInformations
+  updateDeliveryInformation: (newAddress: DeliveryInformations) => void
 }
 
 export const CoffeContext = createContext({} as CoffeContextType)
 
 export function CoffeContextProvider({ children }: ContextProviderProps) {
   const [shoppingCart, setShoppingCart] = useState<CartItem[]>([])
+  const [deliveryInfo, setDeliveryInfo] = useState<DeliveryInformations>(
+    {} as DeliveryInformations,
+  )
 
   const addItemToShoppingCart = (newItem: CartItem) => {
     const isNewItemExistInCart = shoppingCart.find(
@@ -39,8 +55,21 @@ export function CoffeContextProvider({ children }: ContextProviderProps) {
     }
   }
 
+  const updateDeliveryInformation = (
+    newDeliveryInformation: DeliveryInformations,
+  ) => {
+    setDeliveryInfo((_state) => ({ ...newDeliveryInformation }))
+  }
+
   return (
-    <CoffeContext.Provider value={{ shoppingCart, addItemToShoppingCart }}>
+    <CoffeContext.Provider
+      value={{
+        shoppingCart,
+        addItemToShoppingCart,
+        deliveryInfo,
+        updateDeliveryInformation,
+      }}
+    >
       {children}
     </CoffeContext.Provider>
   )
